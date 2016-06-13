81e9c81bd7fe4380ce5cfb252634c824c6dba196
mike miller <mikemdev40@gmail.com>
2016-06-12 21:12:08 -0400

    Initial Commit

---
+++ HLSandTextHighlight/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HLSandTextHighlight
+//
+//  Created by Michael Miller on 6/12/16.
+//  Copyright © 2016 MikeMiller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HLSandTextHighlight/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HLSandTextHighlight
+//
+//  Created by Michael Miller on 6/12/16.
+//  Copyright © 2016 MikeMiller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
